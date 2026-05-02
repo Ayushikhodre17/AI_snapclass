fe9fed0b29cec57efb5865c247c7996a8afa2d55
Ayushi Khodre <ayushikhodre33@gmail.com>
2026-05-02 12:13:17 +0530

    Clean commit without venv

---
+++ src/database/db.py
@@ -0,0 +1,121 @@
+import time
+from httpx import RemoteProtocolError
+from src.database.config import supabase
+import bcrypt 
+
+def hash_pass(pwd):
+    return bcrypt.hashpw(pwd.encode(), bcrypt.gensalt()).decode()
+
+def check_pass(pwd, hashed):
+    return bcrypt.checkpw(pwd.encode(), hashed.encode())
+
+
+def check_teacher_exists(username):
+    # Check for unique username, returns false when username is already taken
+    response = supabase.table("teachers").select("username").eq("username", username).execute()
+    return len(response.data) > 0 
+
+
+
+def create_teacher(username, password, name):
+
+    data = { "username" : username, "password": hash_pass(password), "name": name}
+    response = supabase.table("teachers").insert(data).execute()
+    return response.data
+
+
+def teacher_login(username, password):
+    response = supabase.table("teachers").select("*").eq("username", username).execute()
+    if response.data:
+        teacher = response.data[0]
+        if check_pass(password, teacher['password']):
+            return teacher
+    return None
+
+
+def get_all_students():
+    response = supabase.table('students').select("*").execute()
+    return response.data
+
+def create_student(new_name, face_embedding=None, voice_embedding=None):
+    data = {'name': new_name, 'face_embedding':face_embedding, "voice_embedding": voice_embedding}
+    response = supabase.table('students').insert(data).execute()
+    return response.data
+
+
+def create_subject(subject_code, name, section, teacher_id):
+    data = {"subject_code": subject_code, "name": name, "section": section, "teacher_id": teacher_id}
+    response = supabase.table("subjects").insert(data).execute()
+    return response.data
+
+
+
+def get_teacher_subjects(teacher_id):
+    for attempt in range(3):
+        try:
+            # Get subjects
+            response = supabase.table('subjects') \
+                .select("*") \
+                .eq("teacher_id", teacher_id) \
+                .execute()
+
+            subjects = response.data
+
+            # Loop through each subject
+            for sub in subjects:
+                print(sub)
+                # Get number of students
+                students = supabase.table('subject_students') \
+                    .select("*") \
+                    .eq('subject_id', sub.get('id') or sub.get('subject_id') or sub.get('subject_code')) \
+                    .execute()
+
+                sub['total_students'] = len(students.data)
+
+                # Get attendance logs
+                attendance = supabase.table('attendance_logs') \
+                    .select("timestamp") \
+                    .eq('subject_id', sub.get('id') or sub.get('subject_id') or sub.get('subject_code')) \
+                    .execute()
+
+                unique_sessions = len(set(log['timestamp'] for log in attendance.data))
+                sub['total_classes'] = unique_sessions
+
+            return subjects
+
+        except RemoteProtocolError:
+            print("Server disconnected... retrying")
+            time.sleep(2)
+
+    return []
+
+
+def  enroll_student_to_subject(student_id, subject_id):
+    data = {'student_id': student_id, "subject_id": subject_id}
+    response= supabase.table('subject_students').insert(data).execute()
+    return response.data
+
+
+def  unenroll_student_to_subject(student_id, subject_id):
+    response= supabase.table('subject_students').delete().eq('student_id', student_id).eq('subject_id', subject_id).execute()
+    return response.data
+
+
+
+def get_student_subjects(student_id):
+    response = supabase.table('subject_students').select('*, subjects(*)').eq('student_id', student_id).execute()
+    return response.data
+
+
+def get_student_attendance(student_id):
+    response = supabase.table('attendance_logs').select('*, subjects(*)').eq('student_id', student_id).execute()
+    return response.data
+
+
+def create_attendance(logs):
+    response = supabase.table('attendance_logs').insert(logs).execute()
+    return response.data
+
+def get_attendance_for_teacher(teacher_id):
+    response = supabase.table('attendance_logs').select("*, subjects!inner(*)").eq('subjects.teacher_id', teacher_id).execute()
+    return response.data
